7305c28be87eaac8dc318fa4d2891d1fa83aec31
Alejandro Rivero Espejo <alejandroriveroespejo@AirdeAlejandro.technicolor.net>
2026-04-29 11:53:51 +0200

    speed test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className={`${montserrat.variable} font-montserrat antialiased`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
